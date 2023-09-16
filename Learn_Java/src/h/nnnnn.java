efa61b01b029391f034583fab4dbfd66f7518824
Sudhanshu Kr Talan <sudhanshutalan@gmail.com>
2023-09-16 19:30:12 +0530

    updating nnnn.java and adding forloop.java

--- Learn_Java/src/h/nnnnn.java
+++ Learn_Java/src/h/nnnnn.java
@@ -2,7 +2,9 @@ package h;
 
 public class nnnnn {
     public static  void main(String[] args){
-        System.out.println("Hello");
+        for(int i=1;i<=10;i++){
+            System.out.println(9*i);
+        }
     }
 
 }

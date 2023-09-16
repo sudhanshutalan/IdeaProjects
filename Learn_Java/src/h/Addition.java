ab0119a6dd6d2519134fa710860165919129cdac
Sudhanshu Kr Talan <sudhanshutalan@gmail.com>
2023-09-16 19:54:28 +0530

    updating forloop.java and adding Addition.java

---
+++ Learn_Java/src/h/Addition.java
@@ -0,0 +1,9 @@
+package h;
+
+public class Addition {
+    public static void main(String [] args){
+        int a=13;
+        int b=94;
+        System.out.println(a+b);
+    }
+}
--- Learn_Java/src/h/forloop.java
+++ Learn_Java/src/h/forloop.java
@@ -1,5 +1,13 @@
 package h;
 
 public class forloop {
-    public
+    public static void main(String[]args){
+        int i,j,n=9;
+        for(i=0;i<n;i++){
+            for(j=0;j<i;j++){
+                System.out.print("a");
+            }
+        }
+        System.out.println(" ");
+    }
 }
